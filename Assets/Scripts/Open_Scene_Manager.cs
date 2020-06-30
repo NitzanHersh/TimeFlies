ba1f62c3f93927526cc806dfd9f4878b2fe35e8c
NitzanHersh <NitzanHersh@users.noreply.github.com>
2020-06-30 10:10:06 +0300

    created Opening_Animation scene

---
+++ Assets/Scripts/Open_Scene_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Scene_Manager : MonoBehaviour
+{
+
+    void Update()
+    {
+     if(transform.position.y > 5.5f)
+        {
+            Debug.Log("change scene");
+        }
+    }
+}
