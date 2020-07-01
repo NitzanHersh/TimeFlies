bd1192000c93b47555ee5501c8d2de699545c845
AmitBashari <bashari.amit@gmail.com>
2020-07-01 19:17:33 +0300

    DebugMenuWorks!

--- Assets/Scripts/Debug Scripts/ToggleUI.cs
+++ Assets/Scripts/Debug Scripts/ToggleUI.cs
@@ -8,33 +8,31 @@ public class ToggleUI : MonoBehaviour
     public GameObject ShowMenu;
     public GameObject HideMenu;
     public GameObject CamPosMenu;
+    public GameObject Player;
 
-    public void ShowUI(bool newBool)
+    public void ShowUI()
     {
-        if(newBool == true)
-        {
-            GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic = true;
-            Debug.Log("FoundPlayer" + GameObject.FindGameObjectWithTag("Player").transform.position);
-            CamPosMenu.SetActive(true);
-            HideMenu.SetActive(true);
-            ShowMenu.SetActive(false);
-        }
+        //GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic = true;
+        Player.GetComponent<Rigidbody>().isKinematic = true;
+        Debug.Log("FoundPlayer" + Player.transform.position);
+        CamPosMenu.SetActive(true);
+        HideMenu.SetActive(true);
+        ShowMenu.SetActive(false);
     }
 
-    public void HideUI(bool newBool)
-    {
-        if (newBool == true)
-        {
-            Invoke("PlayerGo", 0.5f);
-            ShowMenu.SetActive(true);
-            CamPosMenu.SetActive(false);
-            HideMenu.SetActive(false);
 
-        }
+    public void HideUI()
+    {
+
+        Invoke("PlayerGo", 0.5f);
+        ShowMenu.SetActive(true);
+        CamPosMenu.SetActive(false);
+        HideMenu.SetActive(false);
     }
 
     public void PlayerGo()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic = false;
+        //GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic = false;
+        Player.GetComponent<Rigidbody>().isKinematic = false;
     }
 }
